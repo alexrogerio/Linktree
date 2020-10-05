850a64189646d00b6ef22f74ec1e9360e35e8939
Alex Santos <alexx.rogerio@gmail.com>
2020-10-05 01:53:11 -0300

    Criação da página do usuário

---
+++ app/Models/Link.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Link extends Model
+{
+    use HasFactory;
+    protected $fillable = ['id_link','click_date'];
+    public $timestamps = false;
+
+}
---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PageController;
+
+Route::get('/',[HomeController::class,'index']);
+
+Route::get('/admin', [AdminController::class, 'index']);
+
+Route::get('/{slug}',[PageController::class, 'index']);
